21a6fd5f489a5ac914dfbf7f579fdb69d9de7a6a
Azael R <azael.devel@gmail.com>
2023-11-27 19:21:13 -0800

    save

---
+++ db/mariadb/alpha/default-data-2.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO CatalogItem(id,catalog,number,brief,name,size) VALUES(1058,3,'p-ph','Personal Pepe/Hawai','Pepe/Hawai','Personal');
+INSERT INTO CatalogItem(id,catalog,number,brief,name,size) VALUES(1059,3,'p-pi','Personal Pepe/Ita','Pepe/Ita','Personal');
+INSERT INTO CatalogItem(id,catalog,number,brief,name,size) VALUES(1058,3,'p-p3','Personal Pepe/3c','Pepe/3C','Personal');
+INSERT INTO CatalogItem(id,catalog,number,brief,name,size) VALUES(1058,3,'p-pm','Personal Pepe/Mex','Pepe/Mex','Personal');
